e80697cfb9b2aebaaed49038aed75281b5cbc0b5
VorobevVictor1 <vorobev.viu@phystech.edu>
2021-11-09 19:58:30 +0300

    Доделаны классы объектов солнечной системы.

--- solar_objects.py
+++ solar_objects.py
@@ -9,13 +9,13 @@ class Space_object:
     """
     def __init__(self, parameters):
         self.type = parameters[0]
-        self.drawing_radius = parameters[1]
+        self.drawing_radius = int(parameters[1])
         self.drawing_color = parameters[2]
-        self.mass = parameters[3]
-        self.x = parameters[4]
-        self.y = parameters[5]
-        self.v_x = parameters[6]
-        self.v_y = parameters[7]
+        self.mass = float(parameters[3])
+        self.x = float(parameters[4])
+        self.y = float(parameters[5])
+        self.v_x = float(parameters[6])
+        self.v_y = float(parameters[7])
         self.f_x = 0
         self.f_y = 0
 
